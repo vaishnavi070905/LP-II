b27db3ccf22399b4372ff2455d3b89a838b9b824
Nagpure Vaishnavi Dilip <vaishnaving07@gmail.com>
2026-05-15 00:10:02 +0530

    Implement DFS algorithm in DFS.py

---
+++ BFS-DFS/DFS.py
@@ -0,0 +1,22 @@
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("DFS Traversal:")
+dfs(visited, graph, 'A')
